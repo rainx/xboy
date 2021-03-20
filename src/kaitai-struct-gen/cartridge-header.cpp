d0ed9328ab563ef80511d706bf196aa822ed0c43
Jing Xu <jing.xu@rightcapital.com>
2021-03-20 16:16:37 +0800

    Add generated header file and kaitai c++ runtime

---
+++ src/kaitai-struct-gen/cartridge-header.cpp
@@ -0,0 +1,35 @@
+// This is a generated file! Please edit source .ksy file and use kaitai-struct-compiler to rebuild
+
+#include "cartridge_header.h"
+namespace kaitai-struct-gen {
+
+    cartridge_header_t::cartridge_header_t(kaitai::kstream* p__io, kaitai::kstruct* p__parent, cartridge_header_t* p__root) : kaitai::kstruct(p__io) {
+        m__parent = p__parent;
+        m__root = this;
+        _read();
+    }
+
+    void cartridge_header_t::_read() {
+        m_unknown_space1 = m__io->read_bytes(256);
+        m_entry_point = m__io->read_bytes(4);
+        m_nintendo_logo = m__io->read_bytes(48);
+        m_title = kaitai::kstream::bytes_to_str(m__io->read_bytes(16), std::string("UTF-8"));
+        m_new_licensee_code = m__io->read_u2le();
+        m_sgb_flag = m__io->read_u1();
+        m_cartridge_type = m__io->read_u1();
+        m_rom_size = m__io->read_u1();
+        m_ram_size = m__io->read_u1();
+        m_destination_code = m__io->read_u1();
+        m_old_licensee_code = m__io->read_u1();
+        m_mask_rom_version_number = m__io->read_u1();
+        m_header_checksum = m__io->read_bytes(1);
+        m_global_checksum = m__io->read_u2le();
+    }
+
+    cartridge_header_t::~cartridge_header_t() {
+        _clean_up();
+    }
+
+    void cartridge_header_t::_clean_up() {
+    }
+}
---
+++ src/kaitai-struct-gen/cartridge-header.h
@@ -0,0 +1,63 @@
+#pragma once
+
+// This is a generated file! Please edit source .ksy file and use kaitai-struct-compiler to rebuild
+
+#include "kaitai/kaitaistruct.h"
+#include <stdint.h>
+#include <memory>
+
+#if KAITAI_STRUCT_VERSION < 9000L
+#error "Incompatible Kaitai Struct C++/STL API: version 0.9 or later is required"
+#endif
+namespace kaitai-struct-gen {
+
+    class cartridge_header_t : public kaitai::kstruct {
+
+    public:
+
+        cartridge_header_t(kaitai::kstream* p__io, kaitai::kstruct* p__parent = nullptr, cartridge_header_t* p__root = nullptr);
+
+    private:
+        void _read();
+        void _clean_up();
+
+    public:
+        ~cartridge_header_t();
+
+    private:
+        std::string m_unknown_space1;
+        std::string m_entry_point;
+        std::string m_nintendo_logo;
+        std::string m_title;
+        uint16_t m_new_licensee_code;
+        uint8_t m_sgb_flag;
+        uint8_t m_cartridge_type;
+        uint8_t m_rom_size;
+        uint8_t m_ram_size;
+        uint8_t m_destination_code;
+        uint8_t m_old_licensee_code;
+        uint8_t m_mask_rom_version_number;
+        std::string m_header_checksum;
+        uint16_t m_global_checksum;
+        cartridge_header_t* m__root;
+        kaitai::kstruct* m__parent;
+
+    public:
+        std::string unknown_space1() const { return m_unknown_space1; }
+        std::string entry_point() const { return m_entry_point; }
+        std::string nintendo_logo() const { return m_nintendo_logo; }
+        std::string title() const { return m_title; }
+        uint16_t new_licensee_code() const { return m_new_licensee_code; }
+        uint8_t sgb_flag() const { return m_sgb_flag; }
+        uint8_t cartridge_type() const { return m_cartridge_type; }
+        uint8_t rom_size() const { return m_rom_size; }
+        uint8_t ram_size() const { return m_ram_size; }
+        uint8_t destination_code() const { return m_destination_code; }
+        uint8_t old_licensee_code() const { return m_old_licensee_code; }
+        uint8_t mask_rom_version_number() const { return m_mask_rom_version_number; }
+        std::string header_checksum() const { return m_header_checksum; }
+        uint16_t global_checksum() const { return m_global_checksum; }
+        cartridge_header_t* _root() const { return m__root; }
+        kaitai::kstruct* _parent() const { return m__parent; }
+    };
+}
