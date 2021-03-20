2300237ec431b9b658308efbbd4284e9deb75480
Jing Xu <jing.xu@rightcapital.com>
2021-03-21 00:40:42 +0800

    Add test for the parser of cartridge header

--- test/kaitai-struct-gen/cartridge-header.test.cpp
+++ test/kaitai-struct-gen/cartridge-header.test.cpp
@@ -3,22 +3,26 @@
 
 #include "./tetris.dump.hpp"
 #include "kaitai-struct-gen/cartridge-header.h"
+#include <fstream>
 #include <iostream>
 #include <sstream>
-
+#include <string>
 namespace gen {
 
 TEST(gen_test, test_tetris) {
 
-  //   std::stringstream tetris_rom_stream;
-  //   tetris_rom_stream << __tetris_gb;
-  std::cout << sizeof(__tetris_gb) << std::endl;
-  //   tetris_rom_stream.seekg(0);
-  //   kaitai::kstream ks(&tetris_rom_stream);
-  //   gen::cartridge_header_t cartrigde_header = gen::cartridge_header_t(&ks);
+  std::stringstream tetris_rom_data;
+  // load rom data
+  tetris_rom_data << std::string((char *)(__tetris_gb), __tetris_gb_len);
+  kaitai::kstream ks(&tetris_rom_data);
 
-  //   ASSERT_EQ(cartrigde_header.cartridge_type(), 0);
-  ASSERT_TRUE(true);
+  gen::cartridge_header_t cartrigde_header = gen::cartridge_header_t(&ks);
+
+  ASSERT_EQ(cartrigde_header.cartridge_type(), 0);
+  ASSERT_STREQ("TETRIS", std::string(cartrigde_header.title()).c_str());
+  ASSERT_EQ(cartrigde_header.rom_size(), 0);
+  ASSERT_EQ(cartrigde_header.ram_size(), 0);
+  ASSERT_EQ(cartrigde_header.global_checksum(), 0xbf16);
 }
 
 } // namespace gen
